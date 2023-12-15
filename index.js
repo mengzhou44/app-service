5760268d091904685777c51b605a6146fa69d516
Meng Zhou <Meng.Zhou@aer.ca>
2023-12-14 22:43:24 -0700

    commit

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 require('dotenv').config(); 
 const { BlobServiceClient } = require("@azure/storage-blob");
- 
-console.log(process.env.CONNECTION_STRING)
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.CONNECTION_STRING);
+const express = require("express")
+
+const app = express()
+
+app.get('/', (req, res)=> {
+    res.send("Hello World!")
+})
+
+app.get('/notes', async (req, res)=> {
+    const content = await downloadBlob()
+    res.send(content)
+})
+ 
+const port = 3000 || process.env.PORT
+
+app.listen(port, ()=> {
+    console.log("Server is listening on port "+ port)
+})
 
 async function downloadBlob() {
   const containerClient = blobServiceClient.getContainerClient(process.env.CONTAINER_NAME);
@@ -11,9 +27,9 @@ async function downloadBlob() {
 
   try {
     const blobContent = await blobClient.downloadToBuffer();
-    console.log("Blob content:", blobContent.toString());
+     return blobContent.toString();
   } catch (error) {
-    console.error("Error downloading blob:", error);
+     return "Error downloading blob:";
   }
 }
 
