e76f02bfa06368d38c33f4d450858926da55efdd
Meng Zhou <Meng.Zhou@aer.ca>
2023-12-14 23:12:29 -0700

    troubleshot

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-require('dotenv').config(); 
-const { BlobServiceClient } = require("@azure/storage-blob");
-
-const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.CONNECTION_STRING);
 const express = require("express")
-
 const app = express()
 
 app.get('/', (req, res)=> {
     res.send("Hello World!")
 })
 
-app.get('/notes', async (req, res)=> {
-    const content = await downloadBlob()
-    res.send(content)
-})
- 
 const port = 3000 || process.env.PORT
-
 app.listen(port, ()=> {
     console.log("Server is listening on port "+ port)
 })
 
-async function downloadBlob() {
-  const containerClient = blobServiceClient.getContainerClient(process.env.CONTAINER_NAME);
-  const blobClient = containerClient.getBlobClient(process.env.BLOB_NAME);
 
-  try {
-    const blobContent = await blobClient.downloadToBuffer();
-     return blobContent.toString();
-  } catch (error) {
-     return "Error downloading blob:";
-  }
-}
+// require('dotenv').config(); 
+// const { BlobServiceClient } = require("@azure/storage-blob");
+
+// const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.CONNECTION_STRING);
+
+
+
+// app.get('/notes', async (req, res)=> {
+//     const content = await downloadBlob()
+//     res.send(content)
+// })
+ 
+
+
+
+// async function downloadBlob() {
+//   const containerClient = blobServiceClient.getContainerClient(process.env.CONTAINER_NAME);
+//   const blobClient = containerClient.getBlobClient(process.env.BLOB_NAME);
+
+//   try {
+//     const blobContent = await blobClient.downloadToBuffer();
+//      return blobContent.toString();
+//   } catch (error) {
+//      return "Error downloading blob:";
+//   }
+// }
